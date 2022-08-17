8d302806ef7e41c032491e7986fcc466c140710a
DevExpressExampleBot <fakeEmail>
2022-08-17 13:27:21 +0400

    Source auto update [skip ci]

--- VB/CustomProgressIndicator/Form1.vb
+++ VB/CustomProgressIndicator/Form1.vb
@@ -1,4 +1,3 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
 Imports System.Data.OleDb
 Imports System.IO
@@ -7,44 +6,39 @@ Imports CustomProgressIndicator.nwindDataSetTableAdapters
 Imports DevExpress.Snap.Core.API
 Imports DevExpress.Snap.Core.Services
 Imports DevExpress.Utils
+
 ' ...
-
 Namespace CustomProgressIndicator
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-		Private Shared Function CreateDataSource() As Object
-			Dim dataSource = New nwindDataSet()
-			Dim connection = New OleDbConnection()
-			connection.ConnectionString = My.Settings.Default.nwindConnectionString
+    Public Partial Class Form1
+        Inherits Form
 
-			Dim customers As New CustomersTableAdapter()
-			customers.Connection = connection
-			customers.Fill(dataSource.Customers)
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-			Dim orders As New OrdersTableAdapter()
-			orders.Connection = connection
-			orders.Fill(dataSource.Orders)
+        Private Shared Function CreateDataSource() As Object
+            Dim dataSource = New nwindDataSet()
+            Dim connection = New OleDbConnection()
+            connection.ConnectionString = Properties.Settings.Default.nwindConnectionString
+            Dim customers As CustomersTableAdapter = New CustomersTableAdapter()
+            customers.Connection = connection
+            customers.Fill(dataSource.Customers)
+            Dim orders As OrdersTableAdapter = New OrdersTableAdapter()
+            orders.Connection = connection
+            orders.Fill(dataSource.Orders)
+            Dim bindingSource = New BindingSource()
+            bindingSource.DataSource = dataSource
+            bindingSource.DataMember = "Customers"
+            Return bindingSource
+        End Function
 
-			Dim bindingSource = New BindingSource()
-			bindingSource.DataSource = dataSource
-			bindingSource.DataMember = "Customers"
-			Return bindingSource
-		End Function
-
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-			Me.snapControl1.DataSources.Add(String.Empty, CreateDataSource())
-
-			Me.snapControl1.Options.SnapMailMergeVisualOptions.DataSourceName = String.Empty
-			Dim filename As String = FilesHelper.FindingFileName(AppDomain.CurrentDomain.BaseDirectory, "Data\template.snx", True)
-
-			If File.Exists(filename) Then
-				Me.snapControl1.Document.LoadDocument(filename, SnapDocumentFormat.Snap)
-			End If
-			Me.snapControl1.ReplaceService(Of ISnapMailMergeProgressIndicationService)(New MyProgressIndicationService(Me.snapControl1))
-		End Sub
-	End Class
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            snapControl1.DataSources.Add(String.Empty, CreateDataSource())
+            snapControl1.Options.SnapMailMergeVisualOptions.DataSourceName = String.Empty
+            Dim filename As String = FilesHelper.FindingFileName(AppDomain.CurrentDomain.BaseDirectory, "Data\template.snx", True)
+            If File.Exists(filename) Then snapControl1.Document.LoadDocument(filename, SnapDocumentFormat.Snap)
+            snapControl1.ReplaceService(Of ISnapMailMergeProgressIndicationService)(New MyProgressIndicationService(snapControl1))
+        End Sub
+    End Class
 End Namespace
